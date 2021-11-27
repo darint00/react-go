c5af401fe2242d467cc174480faa565a342b7a3d
darint00 <darint00@gmail.com>
2021-11-27 16:21:03 -0600

    init commit

---
+++ test-app/src/AppContent.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+import { Accordion, Card, Button } from 'react-bootstrap';
+
+export default class AppContent extends Component{
+   render() {
+      return (
+            <p>This is the Content
+               <br/>
+
+            </p>
+
+      );
+   }
+}
